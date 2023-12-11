bf933cecfb9bcb9314407e7bfeed67fc6aa16d41
pusty1830 <126335408+pusty1830@users.noreply.github.com>
2023-12-11 17:44:19 +0530

    second commit

--- Blog/settings.py
+++ Blog/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 import os
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
